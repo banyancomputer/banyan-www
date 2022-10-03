426f572505d6dbf4973cece461184523d3a70033
alex <alexanderscottmiller@gmail.com>
2022-10-03 11:49:28 -0400

    feat: implement page stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,1 +1,8 @@
 import '../styles/globals.sass';
+import { RouterContext } from 'next/dist/shared/lib/router-context';
+
+export const parameters = {
+  nextRouter: {
+    Provider: RouterContext.Provider,
+  },
+};
--- components/layouts/primary/PrimaryLayout.tsx
+++ components/layouts/primary/PrimaryLayout.tsx
@@ -2,16 +2,16 @@ import Head from 'next/head';
 import styles from './PrimaryLayout.module.sass';
 
 export interface IPrimaryLayout {
-  // children: '{{component}}';
+  children: React.ReactNode;
 }
 
-const PrimaryLayout: React.FC<IPrimaryLayout> = ({ children }) => {
+const PrimaryLayout: React.FC<IPrimaryLayout> = (props: IPrimaryLayout) => {
   return (
     <>
       <Head>
         <title>Primary Layout Example</title>
       </Head>
-      <main className={styles.main}>{children}</main>
+      <main className={styles.main}>{props.children}</main>
     </>
   );
 };
